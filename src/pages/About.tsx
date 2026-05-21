1c0f292e0b490695c8284c0d703b239496bd454b
humanflairco-droid <humanflairco@gmail.com>
2026-05-21 09:32:53 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -43,7 +43,7 @@ const About = () => {
                   src={aboutProduct1}
                   alt="Festive dining table set with multiple 1/4 potjie pots, wine, and Christmas decorations"
                   loading="lazy"
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-cover object-top"
                 />
               </div>
               <div className="w-1/2 p-12 reveal-child">
